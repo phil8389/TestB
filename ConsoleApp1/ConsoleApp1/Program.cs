3a84c474cebbb82bfe23d79de120093fd435652d
Phil83 <naush.naush@gmail.com>
2021-02-16 11:08:39 -0800

    update

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hellooo!!!");
+            Console.Write("Hello how are you!!!");
         }
     }
 }
