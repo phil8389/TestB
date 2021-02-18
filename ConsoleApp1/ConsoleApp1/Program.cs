4141c407b829ebdd5a15204fecc971d186acc983
naushad.ali <naush.naush@gmail.com>
2021-02-18 10:40:32 +0400

    ytry

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello how are you!!!update from off!s!s!!!");
+            Console.Write("Hello how are you!!!update from ofdf!s!s!!!");
         }
     }
 }
